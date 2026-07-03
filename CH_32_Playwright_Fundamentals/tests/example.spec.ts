144c878b3544a2245fa62ee03b1706d833b7f8c4
suryanshu1602 <suryanshu1602@gmail.com>
2026-07-03 22:46:14 +0530

    chore: remove CH_32_Playwright_Fundamentals (moved to separate repo)
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

--- CH_32_Playwright_Fundamentals/tests/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-test('Verify the the title of the TTA Cart', async ({ page }) => {
-
-  await page.goto("https://app.thetestingacademy.com/playwright/ttacart/");
-  await expect(page).toHaveTitle("TTACart - Login");
-
-});
